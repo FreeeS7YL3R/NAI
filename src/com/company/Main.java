3d1e082b98db535aee3523c38c30bd00e54ebf2d
Mateusz <pestka.mateusz@gmail.com>
2015-03-18 16:30:58 +0100

    before changing for 2d loop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -145,17 +145,19 @@ public class Main {
 
         System.out.println("distance = " + distance);
 
+        double rezDistanceFunc = 0;
 
+        for (int i = 0; i <= trainingDataArr.length; i++)
 
-        double rezDistanceFunc = calculateDistance(new double[]{5.1, 3.5, 1.4, 0.2}, new double[]{1, 2, 3, 4});
+            rezDistanceFunc = calculateDistance(new double[]{5.1, 3.5, 1.4, 0.2}, new double[]{1, 2, 3, 4});
         System.out.println("rezDistanceFunc = " + rezDistanceFunc);
+
     }
 
-    public static double calculateDistance(double[] array1, double[] array2)
-    {
+    public static double calculateDistance(double[] array1, double[] array2) {
         double Sum = 0.0;
-        for(int i=0;i<array1.length;i++) {
-            Sum = Sum + Math.pow((array1[i]-array2[i]),2.0);
+        for (int i = 0; i < array1.length; i++) {
+            Sum = Sum + Math.pow((array1[i] - array2[i]), 2.0);
         }
         return Math.sqrt(Sum);
     }
